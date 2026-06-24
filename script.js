8f52984d27ca1332ebcb88214abd9af269c9ec80
abdus-devx <you@example.com>
2026-06-24 16:56:52 +0700

    initial: company profile cv massusi herbal utama

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+link.addEventListener('click', e => {
+
+e.preventDefault();
+
+const target = document.querySelector(
+link.getAttribute('href')
+);
+
+target.scrollIntoView({
+behavior:'smooth'
+});
+
+});
+
+});
